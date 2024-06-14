47f66ba82d11d97c8c599b883a9f2a4229398c36
tongsantan <vincente53@outlook.com>
2024-06-14 18:31:48 +0800

    structure added

---
+++ main.py
@@ -0,0 +1,59 @@
+## 7. Update the main.py
+
+from src import logger
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.pipeline.stage_03_training import ModelTrainingPipeline
+from src.pipeline.stage_04_evaluation import EvaluationPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+## 7. Update the main.py
+
+STAGE_NAME = "Prepare base model"
+
+try:
+    logger.info(f"************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+## 7. Update the main.py
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"********************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Evaluation"
+
+try:
+    logger.info(f"********************************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = EvaluationPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
