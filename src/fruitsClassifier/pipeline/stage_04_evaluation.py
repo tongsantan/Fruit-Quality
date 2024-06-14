875781cfdda75f2e897d54b44e0d677efcc4553e
tongsantan <vincente53@outlook.com>
2024-06-14 16:58:40 +0800

    structure added

---
+++ src/fruitsClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,27 @@
+from src.fruitsClassifier.config.configuration import ConfigurationManager
+from src.fruitsClassifier.components.evaluation import Evaluation
+from src.fruitsClassifier import logger
+
+STAGE_NAME = "Evaluation"
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
